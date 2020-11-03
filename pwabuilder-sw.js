43f09279fb033b3f24a974f1ab9a75079316fcb5
Leonanta Pramudya Kusuma <67642134+leonanta@users.noreply.github.com>
2020-11-03 18:14:48 +0700

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -22,7 +22,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll(offlineFallbackPage))
   );
 });
 
